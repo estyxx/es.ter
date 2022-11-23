7e2992d9748b871089ad0e5b72c55f814007e06c
Etty <beltrami.ester@gmail.com>
2022-11-23 11:37:08 +0000

    Add PyCon Italia Talk

--- next.config.js
+++ next.config.js
@@ -5,11 +5,16 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/europython',
-        destination: 'https://youtu.be/7_FyRR3yN-k',
+        source: "/europython",
+        destination: "https://youtu.be/7_FyRR3yN-k",
         permanent: true,
       },
-    ]
+      {
+        source: "/pyconitalia",
+        destination: "https://youtu.be/YHEaPloOaSA",
+        permanent: true,
+      },
+    ];
   },
 };
 
